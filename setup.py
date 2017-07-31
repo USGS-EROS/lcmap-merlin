25319d11bb79930ad8f5bdc944db2fba4fe961ed
davidvhill <davehill75@gmail.com>
2017-07-31 15:58:05 -0500

    initial commit for lcmap-merlin version 0.5.  All tests pass & readme is updated with examples.

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='lcmap-merlin',
+      version='0.5',
+      description='Python client library for LCMAP-Aardvark',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Unlicense',
+        'Programming Language :: Python :: 3.6',
+      ],
+      keywords='usgs lcmap eros',
+      url='http://github.com/usgs-eros/lcmap-merlin',
+      author='USGS EROS LCMAP',
+      author_email='',
+      license='Unlicense',
+      packages=['merlin'],
+      install_requires=[
+          'numpy',
+          'requests',
+          'python-dateutil',
+      ],
+      # List additional groups of dependencies here (e.g. development
+      # dependencies). You can install these using the following syntax,
+      # for example:
+      # $ pip install -e .[test]
+      extras_require={
+          'test': ['pytest',
+                   'hypothesis',
+                   'mock',
+                  ],
+          'dev': ['jupyter',],
+      },
+      # entry_points={
+          #'console_scripts': [''],
+      # },
+      include_package_data=True,
+      zip_safe=False)
