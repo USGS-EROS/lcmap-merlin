4ec9feca9c51b0ca8cbf7b815437fd3009e3f7b9
davidvhill <david.hill.ctr@usgs.gov>
2017-08-01 16:22:15 -0500

    slimmed down test data to two years worth to come in under pypi file size limits

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 
 setup(name='lcmap-merlin',
-      version='0.5.4',
+      version='0.5.5',
       description='Python client library for LCMAP-Aardvark',
       long_description=readme(),
       classifiers=[
