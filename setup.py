97e51d69fb0fa92cb044a975bf1447b83a1f9c3f
davidvhill <davehill75@gmail.com>
2017-08-01 13:50:48 -0500

    added find_packages() to setup.py to correct missing merlin.support package

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 
 setup(name='lcmap-merlin',
-      version='0.5.2',
+      version='0.5.3',
       description='Python client library for LCMAP-Aardvark',
       long_description=readme(),
       classifiers=[
