179c92eca1da590e20aa48018f3a01eec110f2e7
davidvhill <davehill75@gmail.com>
2017-11-04 14:12:50 -0500

    updated version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 
 setup(name='lcmap-merlin',
-      version='1.0',
+      version='1.0.1',
       description='Python client library for LCMAP-Aardvark',
       long_description=readme(),
       classifiers=[
