828ca43927e6aacfdd2c5eeef4ab91df7272fae9
davidvhill <davehill75@gmail.com>
2017-11-07 14:19:56 -0600

    corrected file modes in files.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 
 setup(name='lcmap-merlin',
-      version='1.0.2',
+      version='1.0.3',
       description='Python client library for LCMAP-Aardvark',
       long_description=readme(),
       classifiers=[
