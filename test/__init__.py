a8238276ce0ddb3c5551e79385d9a91532c2a073
davidvhill <davehill75@gmail.com>
2017-12-22 17:05:09 -0600

    moved cassette to chipmunk-ard-v1.yaml to support future versions.

--- test/__init__.py
+++ test/__init__.py
@@ -8,5 +8,5 @@ y_nodata = 302805.0
 acquired = '2010/2013'
 profile = 'chipmunk-ard'
 env = {'CHIPMUNK_URL': 'http://localhost:5656'}
-cassette = 'test/resources/cassette.yaml'
+cassette = 'test/resources/chipmunk-ard-v1-cassette.yaml'
 vcr = _vcr.VCR(record_mode='new_episodes')
